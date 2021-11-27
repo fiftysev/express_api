605ab92d818335816cba4049288d9bbf3e0b46f7
fiftysev <nesgorai@bk.ru>
2021-11-27 13:44:12 +1000

    Add dotenv, fix const using

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+require("dotenv").config();
 import express from "express";
 import bodyParser from "body-parser";
 import userRouter from "./src/routers/userRouter";
@@ -6,10 +7,8 @@ import helperRouter from "./src/routers/helperRouter";
 
 const app = express();
 
-const PORT = 3000;
-
 // middlewares for parsing req.body
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // add routers
@@ -18,16 +17,10 @@ app.use("/users", userRouter);
 app.use("/tweets", tweetRouter);
 app.use("/api", helperRouter);
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Main route");
-})
+});
 
-//TODO: Роут для актуального
-//TODO: Написать запрос на получение списка актуального
-
-//TODO: Роут для поиска
-//TODO: Написать запрос на поиск среди твиттов (просто по совпадению куска текста)
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
